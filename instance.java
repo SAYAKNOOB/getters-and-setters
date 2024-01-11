e162463e0a65a2b0c00a2e319908d4d1022df331
SAYAK SEN <89062032+SAYAKNOOB@users.noreply.github.com>
2024-01-11 20:32:36 +0530

    Add files via upload

---
+++ instance.java
@@ -0,0 +1,39 @@
+class instance {
+
+
+    Float a;
+
+    void setval(float a)
+    {
+        this.a = a;
+    }
+
+    void getter() {
+        System.out.println(a);
+    }
+
+    void square() {
+
+        System.out.println(a);
+    }
+
+    public static void main(String args[]) {
+        instance m =new instance();
+        instance n =new instance();
+
+        m.getter();
+
+        n.getter();
+
+        m.setval(10);
+
+        n.setval(12);
+
+        m.getter();
+
+        n.getter();
+
+        m.square();
+        n.square();
+    }
+}
